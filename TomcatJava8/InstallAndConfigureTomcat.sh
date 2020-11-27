1e35b89cd69bc5642880273df6c42a0a043713ee
Yasub Hashmi <yas.hashmi@gmail.com>
2020-11-27 22:50:49 +0530

    Some clean up and sorting

---
+++ TomcatJava8/InstallAndConfigureTomcat.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+sudo groupadd tomcat
+
+sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
+cd /tmp
+
+curl -O https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.60/bin/apache-tomcat-8.5.60.tar.gz
+
+sudo mkdir /opt/tomcat
+sudo tar xzvf apache-tomcat-8.5.60.tar.gz -C /opt/tomcat --strip-components=1
+
+
+cd /opt/tomcat
+
+sudo chgrp -R tomcat /opt/tomcat
+
+
+sudo chmod -R g+r conf
+sudo chmod g+x conf
+
+
+sudo chown -R tomcat webapps/ work/ temp/ logs/
+
+sudo su; su tomcat
+
+cp /home/vagrant/tomcat/tomcat-users.xml /opt/tomcat/conf/
+cp /home/vagrant/tomcat/manager/context.xml /opt/tomcat/webapps/manager/META-INF/
+cp /home/vagrant/tomcat/host-manager/context.xml /opt/tomcat/webapps/host-manager/META-INF/
+
+
+sudo cp /home/vagrant/tomcat/tomcat.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl start tomcat
+sudo systemctl status tomcat
+sudo ufw allow 8080
+
+
---
+++ Ubuntu18_Java8/InstallJava.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+sudo apt update -y
+sudo apt-get upgrade -y
+
+sudo apt install openjdk-8-jre -y
+sudo apt install openjdk-8-jdk -y
+
+sudo apt install maven -y
