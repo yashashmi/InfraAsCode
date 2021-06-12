608a5341ed6cbcbb4dcb15b1a14031e91ed1fdb7
Yasub Hashmi <yas.hashmi@gmail.com>
2021-06-12 22:16:39 +0530

    Updating the tomcat apahce installation link

--- TomcatJava8/InstallAndConfigureTomcat.sh
+++ TomcatJava8/InstallAndConfigureTomcat.sh
@@ -5,10 +5,10 @@ sudo groupadd tomcat
 sudo useradd -s /bin/false -g tomcat -d /opt/tomcat tomcat
 cd /tmp
 
-curl -O https://mirrors.estointernet.in/apache/tomcat/tomcat-8/v8.5.60/bin/apache-tomcat-8.5.60.tar.gz
+curl -O https://archive.apache.org/dist/tomcat/tomcat-8/v8.5.66/bin/apache-tomcat-8.5.66.tar.gz
 
 sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-8.5.60.tar.gz -C /opt/tomcat --strip-components=1
+sudo tar xzvf apache-tomcat-8.5.66.tar.gz -C /opt/tomcat --strip-components=1
 
 
 cd /opt/tomcat
